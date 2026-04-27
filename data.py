08f628fdc2e95b2d6e8fc5147bb35e8e976a8cb5
TangentCN <OhImT@outlook.com>
2026-04-27 17:10:00 +0800

    改子进程数，只使用主进程

--- data.py
+++ data.py
@@ -22,13 +22,13 @@ def get_data_loaders():
                                             download=True, transform=transform)
     # 创建训练数据加载器
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                            shuffle=True, num_workers=2)
+                                            shuffle=True, num_workers=0)
     #加载CIFAR-10测试集
     testset = torchvision.datasets.CIFAR10(root=dataset_path, train=False,
                                         download=True, transform=transform)
     # 创建测试数据加载器
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                            shuffle=False, num_workers=2)
+                                            shuffle=False, num_workers=0)
     return trainloader, testloader
 
 def get_path():
