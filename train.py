c4997a0b90b132bc1d8d4b2bdfa69cd2f1463793
TangentCN <OhImT@outlook.com>
2026-04-27 17:30:18 +0800

    图表改名以符合语法

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def draw(losses):
     plt.style.use('seaborn-v0_8-darkgrid')      # 使用样式
     fig, ax = plt.subplots()                    # 初始化界面fig与图表ax
     ax.plot(inputs, losses, linewidth = 3)      # 指定输入输出，粗细
-    ax.set_title('Loss in Training Process', fontsize=24)   # 标题
+    ax.set_title('Loss in the Training Process', fontsize=24)   # 标题
     ax.set_xlabel('Time / (50 batches)', fontsize=14)     # x标题
     ax.set_ylabel('Loss', fontsize=14)                      # y标题
     ax.tick_params(axis='both', labelsize=14)               # 刻度
