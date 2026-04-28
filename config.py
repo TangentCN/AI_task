ff31a95c6e3f6fb6c2a03b37c9d94646c412dc43
TangentCN <OhImT@outlook.com>
2026-04-28 15:18:09 +0800

    test模块同步更改

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     batch_size = 128
-    epochs = 100
-    learning_rate = 0.001
+    epochs = 50
+    learning_rate = 0.01
     momentum = 0.9
     dataset_path = 'D:/CIFAR_dataset'
     dropout_rate = 0.5
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 from data import get_data_loaders, get_path
-from model import Net
+from model import LeNet, Dropout_LeNet
 from config import Config
 
 def predict(testloader, net, device='cpu'):
@@ -89,7 +89,7 @@ def predict(testloader, net, device='cpu'):
 
 def load_model(weight_path, device='cpu'):
     '''加载训练好的模型'''
-    net = Net()
+    net = Dropout_LeNet()
     # 加载权重参数（需要路径）
     net.load_state_dict(torch.load(weight_path, map_location=device))
     # 选择设备
