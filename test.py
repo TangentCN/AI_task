046e17c0c99a571950c58cbae57916e136286d8d
TangentCN <OhImT@outlook.com>
2026-04-29 20:55:02 +0800

    删掉没用的变量

--- test.py
+++ test.py
@@ -148,7 +148,6 @@ def run_test():
     # 2.获取测试集
     _, _, test_loader = get_data_loaders()
     # 3.获取模型权重参数（用于模型初始化）
-    epoch = cfg.epochs
     weight_path = f"{get_path()}/best_model.pth"
     # 4.模型初始化
     net = load_model(cfg, weight_path, device)
