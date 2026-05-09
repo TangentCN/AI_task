5524c43c405c374543674d096c56417a99b25847
TangentCN <OhImT@outlook.com>
2026-05-09 19:50:20 +0800

    最终改动

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    model_name = 'VGG11_CIFAR10'
+    model_name = 'AlexNet_CIFAR10'
     '''
     model_names:
         'LeNet'
@@ -11,13 +11,13 @@ class Config:
     epochs = 100
     dataset_path = 'D:/CIFAR_dataset'
 
-    learning_rate = 0.001
-    momentum = 0.9
-    weight_decay = 0
-    dropout_rate = 0.5
+    learning_rate = 0.01
+    momentum = 0.95
+    weight_decay = 0.0003
+    dropout_rate = 0.6
 
     factor = 0.5
-    patience = 5
+    patience = 4
     threshold = 1e-4
 
-    early_stopping_patience = 10 # 衰减器的耐心不要大于早停耐心
+    early_stopping_patience = 7 # 衰减器的耐心不要大于早停耐心
